27e553bd09c8de5090b129fbddffad172bfee5ba
Yunji Kim <yj1230k@gmail.com>
2023-12-15 13:45:29 -0500

    Dump in step function

--- compiler/rustc_const_eval/src/interpret/dump.rs
+++ compiler/rustc_const_eval/src/interpret/dump.rs
@@ -1,4 +1,9 @@
-// use crate::const_eval::CompileTimeEvalContext;
+use crate::const_eval::CompileTimeEvalContext;
+use super::InterpCx;
+// use super::InterpResult;
+use super::Machine;
+
+use either::Either;
 // use rustc_middle::query::TyCtxtAt;
 use std::path::Path;
 use rustc_middle::mir::Body;
@@ -14,7 +19,7 @@ use rustc_hir::definitions::{DefPath, DisambiguatedDefPathData};
 // use ansi_term::Colour;
 use colored::Colorize;
 
-pub fn dump_in_eval_query(
+pub fn dump_in_eval_query( // eval_queries.rs => DUMP_ON
     tcx: TyCtxt<'_>,
     body: &Body<'_>,
     outdir: &Path,
@@ -66,7 +71,28 @@ pub fn dump_in_eval_query(
     // file.write_all(content.as_bytes()).expect("Fail to write file.");
 }
 
-pub fn dump_in_step(
+pub fn bb_dump<'mir, 'tcx>(
+    ecx: &CompileTimeEvalContext<'mir, 'tcx>
+) {
+    
+    // let bbs = body.basic_blocks.as_mut();
+    let loc= ecx.frame().loc;
+    // let bb_id = ecx.frame().loc.left();
+    if let Either::Left(l_loc) = loc {
+        let block = l_loc.block;
+        let statement_idx = l_loc.statement_index;
+        println!("[s][{:?}][{:?}]\n", block, statement_idx);
+        // let bb_id = 
+        // info!("// executing {:?}", loc.block);
+    }
+    // self.frame_mut().loc.as_mut().left().unwrap().statement_index += 1;
+
+    // let bbs = &body.basic_blocks;
+    // println!("{:?}", bbs);
+}
+
+
+pub fn dump_in_step( // step.rs => STEP
     // ecx: &CompileTimeEvalContext<'mir, 'tcx>
     tcx: TyCtxt<'_>,
     body: &Body<'_>,
@@ -76,3 +102,69 @@ pub fn dump_in_step(
     let outdir = std::path::PathBuf::from("/home/y23kim/aptos-core/third_party/move/move-bytecode-verifier/src/regression_tests/dump_yj");
     dump_in_eval_query(tcx, body, &outdir);
 }
+
+impl<'mir, 'tcx: 'mir, M: Machine<'mir, 'tcx>> InterpCx<'mir, 'tcx, M> {
+    #[inline(always)]
+    pub fn bb_dump_in_step(&mut self) { // step.rs => STEPP
+        // Implementation of the function to dump basic blocks
+        // Access fields and methods of InterpCx using `self`
+        // ...
+        if let Some(last) = self.stack().last() {
+            // crate information
+            let body = self.body();
+            // DIFF BETWEEN TyCtxt and TyXtxtAt
+            let tcx = self.tcx; // self.tcx.tcx 
+            let instance_def = body.source.instance;
+            let def_id: DefId = instance_def.def_id();
+            let crate_name2 = tcx.crate_name(def_id.krate);
+            let s1 = format!("[{:?}]", crate_name2);
+            print!("{}", s1.red());
+        
+            let def_kind: DefKind = tcx.def_kind(def_id);
+            let s2 = format!("[{:?}]", def_kind);
+            print!("{}", s2.blue());
+        
+            let def_path: DefPath = tcx.def_path(def_id);
+            let def_paths: Vec<DisambiguatedDefPathData> = def_path.data;
+            for item in &def_paths {
+                let s3 = format!("[{:?}][{:?}]", item.data, item.disambiguator);
+                print!("{}", s3.green());
+            }
+            println!("");
+
+            // BASIC BLOCK and statement number
+            let loc = last.loc;
+
+            if let Either::Left(l_loc) = loc {
+                let block = l_loc.block;
+                let statement_idx = l_loc.statement_index;
+                println!("[{:?}][{:?}]", block, statement_idx);
+                // let bb_id = 
+                // info!("// executing {:?}", loc.block);
+            }
+
+        }
+        // let loc= self.frame().loc;
+        // // let bb_id = ecx.frame().loc.left();
+        // if let Either::Left(l_loc) = loc {
+        //     let block = l_loc.block;
+        //     let statement_idx = l_loc.statement_index;
+        //     println!("[{:?}][{:?}]\n", block, statement_idx);
+        //     // let bb_id = 
+        //     // info!("// executing {:?}", loc.block);
+        // }
+    }
+}
+
+// pub fn bb_dump_in_step<'mir, 'tcx>(
+//     ecx: &CompileTimeEvalContext<'mir, 'tcx>
+// ) {
+    
+//     // let bbs = body.basic_blocks.as_mut();
+
+//     // self.frame_mut().loc.as_mut().left().unwrap().statement_index += 1;
+
+//     // let bbs = &body.basic_blocks;
+//     // println!("{:?}", bbs);
+
+// }
