d8c30ebdc1a9ff46749b36dc3f4af7053eb88c26
Yunji Kim <yj1230k@gmail.com>
2024-01-24 16:48:54 -0500

    Call-Return paired

--- compiler/rustc_const_eval/src/const_eval/eval_queries.rs
+++ compiler/rustc_const_eval/src/const_eval/eval_queries.rs
@@ -74,7 +74,12 @@ fn eval_body_using_ecx<'mir, 'tcx, R: InterpretationResult<'tcx>>(
         cid.promoted.map_or_else(String::new, |p| format!("::{p:?}"))
     );
 
-    ecx.yj_push(String::from("[maybeCall"));
+    // correct call type
+    // 1. push_stack_frame in eval_body_using_ecx
+    // 2. push_stack_frame in eval_terminator, Call kind
+    ecx.yj_push(String::from("[evalbodyCall"));
+    let s = ecx.fn_info(body);
+    ecx.yj_push(s);
     ecx.push_stack_frame(
         cid.instance,
         body,
